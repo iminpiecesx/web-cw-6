8a3b1612eaab67d78ca45bd6e58d938a952c9a70
Afrah79 <afrahmubarakalazmi@gmail.com>
2021-01-03 23:17:37 +0300

    DONE

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,18 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+
+
+
+function sayHello(name){
+    
+ if (name != null ){
+ console.log(`مرحباً, ${name}`)
+ }
+  else {
+  console.log('يرجى إدخال الأسم ')   
+}
+}
+
+sayHello('afrah');
+sayHello();
+
+
